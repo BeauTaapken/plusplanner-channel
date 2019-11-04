12b04e64c5c8395698d5c5618274e65f54363619
BeauTaapken <beautaapken@me.com>
2019-11-04 10:00:21 +0100

    * changed test folder

--- src/test/java/plus/planner/channel/channelApplicationTests.java
+++ src/test/java/plus/planner/channel/channelApplicationTests.java
@@ -1,4 +1,4 @@
-package plus.planner.api.plusplannerapi;
+package plus.planner.channel;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PlusplannerapiApplicationTests {
+public class channelApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+//    @Test
+//    public void contextLoads() {
+//    }
 
 }
