14d7c6e1d695b5c4af0b932895310d854a5e6704
Niek125 <niek.sleddens@gmail.com>
2020-01-23 22:29:42 +0100

    code smells

--- src/main/java/plus/planner/channel/controllers/ChannelController.java
+++ src/main/java/plus/planner/channel/controllers/ChannelController.java
@@ -1,5 +1,6 @@
 package plus.planner.channel.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,26 +8,32 @@ import org.springframework.web.bind.annotation.*;
 import plus.planner.channel.model.Channel;
 import plus.planner.channel.repository.ChannelRepository;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("channel")
 public class ChannelController {
     private final Logger logger = LoggerFactory.getLogger(ChannelController.class);
     private final ChannelRepository repo;
+    private final ObjectMapper objectMapper;
 
     @Autowired
-    public ChannelController(ChannelRepository repo) {
+    public ChannelController(ChannelRepository repo, ObjectMapper objectMapper) {
         this.repo = repo;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping(path = "/create")
-    public void createChannel(@RequestBody Channel channel) {
+    public void createChannel(@RequestBody String chnl) throws IOException {
+        final Channel channel = objectMapper.readValue(chnl, Channel.class);
         logger.info("saving channel: {}", channel.getChannelid());
         repo.save(channel);
         logger.info("saved channel");
     }
 
     @PostMapping(path = "/update")
-    public void updateChannel(@RequestBody Channel channel) {
+    public void updateChannel(@RequestBody String chnl) throws IOException {
+        final Channel channel = objectMapper.readValue(chnl, Channel.class);
         logger.info("updating channel: {}", channel.getChannelid());
         repo.save(channel);
         logger.info("updated channel");
